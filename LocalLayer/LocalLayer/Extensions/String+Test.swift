7862b2e128cd0e6a00fb481d89cf76110d67ea88
madhusudhanivy <madhusudhan.putta@ivycomptech.com>
2021-07-09 22:35:08 +0530

    initial commit

---
+++ LocalLayer/LocalLayer/Extensions/String+Test.swift
@@ -0,0 +1,9 @@
+//
+//  UIImage.swift
+//  LocalLayer
+//
+//  Created by Madhusudhan Reddy Putta on 09/07/21.
+//
+
+import Foundation
+import UIKit
---
+++ LocalLayer/LocalLayer/LocalLayer.h
@@ -0,0 +1,18 @@
+//
+//  LocalLayer.h
+//  LocalLayer
+//
+//  Created by Madhusudhan Reddy Putta on 09/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocalLayer.
+FOUNDATION_EXPORT double LocalLayerVersionNumber;
+
+//! Project version string for LocalLayer.
+FOUNDATION_EXPORT const unsigned char LocalLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalLayer/PublicHeader.h>
+
+
